037fe577207e6031a0298fb15c396d9cb0d052c3
neokoni <dulin081682@126.com>
2025-08-20 00:26:00 +0800

    init project

---
+++ src/main/java/ink/neokoni/env4u/Env4U.java
@@ -0,0 +1,17 @@
+package ink.neokoni.env4u;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Env4U extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
